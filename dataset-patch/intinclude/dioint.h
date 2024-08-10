0748a90f032e9b9a4ab952d3e438bb4fc2352839
Mike Fulton <mikefultonpersonal@gmail.com>
2024-08-09 20:55:34 -0400

    basic processing for dd allocation for read
    Signed-off-by: Mike Fulton <mikefultonpersonal@gmail.com>

--- dataset-patch/intinclude/dioint.h
+++ dataset-patch/intinclude/dioint.h
@@ -1,7 +1,13 @@
 #ifndef __DIOINT__
   #define __DIOINT__ 1
 
+  #define DD_MAX (8)
+  #define DS_MAX (44)
+  #define MEM_MAX (8)
   struct DIFILE {
-    char ddname[8+1];
+    char dataset_full_name[DS_MAX+MEM_MAX+2+1];
+    char dataset_name[DS_MAX+1];
+    char member_name[MEM_MAX+1];
+    char ddname[DD_MAX+1];
   };
 #endif
